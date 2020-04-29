5b734889ecd8e76e76e8f78520d3934bd16c98a5
yaosiang.su <yaosiang.su@104.com.tw>
2020-04-29 08:00:18 +0800

    add description for sha demo

--- 01_hash/sha.ts
+++ 01_hash/sha.ts
@@ -5,6 +5,7 @@ import { v4 } from 'uuid';
 demo1();
 demo2();
 
+// 如何用依賴套件計算各種 SHA 值
 function demo1(): void {
   const data: string = v4();
   console.log(`Text     : ${data}`);
@@ -15,6 +16,7 @@ function demo1(): void {
   console.log(`SHA3-512 : ${sha3_512(data)}`);
 }
 
+// 些微的輸入訊息變化，產生的 SHA 值會有很大的差異
 function demo2(): void {
   const data: string = v4();
   const modifiedData: string = `@${data.substring(1)}`;
