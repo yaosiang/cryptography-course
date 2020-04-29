d220678b628af83e9d1f104150ef3426f6d2b8b7
yaosiang.su <yaosiang.su@104.com.tw>
2020-04-29 10:58:53 +0800

    update hmac example

--- 02_mac/hmac.ts
+++ 02_mac/hmac.ts
@@ -2,14 +2,22 @@ import * as crypto from 'crypto';
 import { v4 } from 'uuid';
 
 const key: string = 'secret key';
+const wrongKey: string = `@${key.substring(1)}`;
 const data: string = v4();
+const wrongData: string = `@${data.substring(1)}`;
 const digest: string = hmacWithSha256(data, key);
-const wrongDigest: string = digest.substring(1);
+const wrongDigest: string = hmacWithSha256(data, wrongKey);
 
+console.log(`Key                         : ${key}`);
 console.log(`Text                        : ${data}`);
+console.log(`Wrong Key                   : ${wrongKey}`);
+console.log(`Wrong Text                  : ${wrongData}`);
 console.log(`HMAC Digest                 : ${digest}`);
+console.log(`Wrong HMAC Digest           : ${wrongDigest}`);
+console.log(`Verify with correct text    : ${verifyHmacWithSha256(data, digest, key)}`);
 console.log(`Verify with correct digest  : ${verifyHmacWithSha256(data, digest, key)}`);
-console.log(`Verify with modified digest : ${verifyHmacWithSha256(data, wrongDigest, key)}`);
+console.log(`Verify with modified text   : ${verifyHmacWithSha256(wrongData, digest, key)}`);
+console.log(`Verify with modified key    : ${verifyHmacWithSha256(data, wrongDigest, key)}`);
 
 function hmacWithSha256(data: string, key: string): string {
   const hmac: crypto.Hmac = crypto.createHmac('sha256', key);
