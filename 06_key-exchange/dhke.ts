b193fb9fb2bd46edc9e5887200e8df71d5ac3f7c
yaosiang.su <yaosiang.su@104.com.tw>
2020-05-05 19:11:24 +0800

    update DHKE example

--- 06_key-exchange/dhke.ts
+++ 06_key-exchange/dhke.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto';
 import * as assert from 'assert';
 
-const alice: crypto.DiffieHellman = crypto.createDiffieHellman(1024);
+const alice: crypto.DiffieHellman = crypto.createDiffieHellman(8);
 const primeFromAlice: string = alice.getPrime('hex');
 const keyFromAlice = alice.generateKeys('hex');
 console.log('Key Exchange Agreements for Alice:');
@@ -15,24 +15,25 @@ logDetails(bob);
 const secret1 = alice.computeSecret(keyFromBob, 'hex', 'hex');
 const secret2 = bob.computeSecret(keyFromAlice, 'hex', 'hex');
 assert.strictEqual(secret1, secret2);
-console.log(`Secret: ${secret1}`);
+console.log(`Shared Secret: ${parseInt(secret1, 16)}`);
 
+// http://www.irongeek.com/diffie-hellman.php
 function logDetails(dh: crypto.DiffieHellman): void {
   let keyExchangeArguments: any = {
-    prime_number: null,   // which is q in our notation
-    primitive_root: null, // which is t in our notation
-    x: null,              // the only secret parameter for each of its participant
-    y: null,              // the public key which each of participant exchange with other part
+    primeNumber: null,   // which is p
+    primitiveRoot: null, // which is g
+    privateX: null,      // the only secret parameter for each of its participant
+    publicY: null,       // the public key which each of participant exchange with other part
   };
 
-  keyExchangeArguments.prime_number   = dh.getPrime().toString('hex')
-  keyExchangeArguments.primitive_root = dh.getGenerator().toString('hex')
-  keyExchangeArguments.x              = dh.getPrivateKey().toString('hex')
-  keyExchangeArguments.y              = dh.getPublicKey().toString('hex')
+  keyExchangeArguments.primeNumber   = dh.getPrime().toString('hex')
+  keyExchangeArguments.primitiveRoot = dh.getGenerator().toString('hex')
+  keyExchangeArguments.privateX      = dh.getPrivateKey().toString('hex')
+  keyExchangeArguments.publicY       = dh.getPublicKey().toString('hex')
 
-  console.log(`  - Prime Number   : ${keyExchangeArguments.prime_number}`);
-  console.log(`  - Primitive Root : ${keyExchangeArguments.primitive_root}`);
-  console.log(`  - X              : ${keyExchangeArguments.x}`);
-  console.log(`  - Y              : ${keyExchangeArguments.y}`);
+  console.log(`  - Prime Number   : ${parseInt(keyExchangeArguments.primeNumber, 16)}`);
+  console.log(`  - Primitive Root : ${parseInt(keyExchangeArguments.primitiveRoot, 16)}`);
+  console.log(`  - Private X      : ${parseInt(keyExchangeArguments.privateX, 16)}`);
+  console.log(`  - Public Y       : ${parseInt(keyExchangeArguments.publicY, 16)}`);
   console.log();
 }
